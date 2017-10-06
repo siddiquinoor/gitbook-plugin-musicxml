80a4fe38a6da52d5730aac28be49510ff9f4c533
Yiu Man Chi <ymcatar@gmail.com>
2017-10-07 00:54:14 +0800

    init commit

---
+++ assets/musicxml.js
@@ -0,0 +1,21 @@
+require(["gitbook", "jquery"], function (gitbook, $) {
+    var onLoad = function() {
+        // var osmd = new opensheetmusicdisplay.OSMD("container-id");
+        $('.musicxml').each(function() {
+            var id = $(this).attr('id');
+            var osmd = new opensheetmusicdisplay.OSMD(id);
+
+            var xml = $(this).data('url');
+            osmd.load(xml)
+                .then(function() {
+                    osmd.zoom = 0.55;
+                    osmd.render();
+                })
+                .catch(function() {
+                    console.error(err);
+                });
+        });
+    }
+  
+    gitbook.events.bind("page.change", onLoad);
+  });
